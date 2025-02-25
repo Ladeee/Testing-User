30ff436d0eb61a0ee96613c3227742f85e654945
OmoladeTrust <ladeakingboye@gmail.com>
2025-02-25 18:36:16 +0100

    solved netlify building failure

--- src/components/users/index.tsx
+++ src/components/users/index.tsx
@@ -1,4 +1,3 @@
-import {useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import axios from 'axios'
@@ -15,58 +14,51 @@ interface User {
   phone: number
 }
 
-const USERS_URL = process.env.REACT_APP_USERS_API;
+const USERS_URL = process.env.REACT_APP_USERS_API
 
 const fetchUsers = async (): Promise<User[]> => {
-  console.log("API URL:", USERS_URL);
-  const { data } = await axios.get(`${USERS_URL}`);
-  console.log(data);
-  return data;
-};
+  console.log('API URL:', USERS_URL)
+  const { data } = await axios.get(`${USERS_URL}`)
+  console.log(data)
+  return data
+}
 
 const useUsers = () => {
   return useQuery({
     queryKey: ['users'],
     queryFn: fetchUsers,
-    retry: false
+    retry: false,
   })
 }
 
 export default function Users() {
   const { data: users, isLoading, error } = useUsers()
-  const [searchParams] = useSearchParams();
-  const searchQuery = searchParams.get("search") || ""
+  const [searchParams] = useSearchParams()
+  const searchQuery = searchParams.get('search') || ''
   const navigate = useNavigate()
 
-  
   if (isLoading) return <p>Loading users...</p>
-  if (error) {console.log('error:', error); return <p>Error loading users.</p>}
-
+  if (error) {
+    console.log('error:', error)
+    return <p>Error loading users.</p>
+  }
 
   const filteredUsers = users?.length
     ? users.filter((user) =>
         `${user.firstName} ${user.lastName} ${user.email}`
           .toLowerCase()
-          .includes(searchQuery.toLowerCase())
+          .includes(searchQuery.toLowerCase()),
       )
-    : [];
-
+    : []
 
   const handleUserClick = (user: User) => {
-    navigate(`/user-details/${user.id}`);
+    navigate(`/user-details/${user.id}`)
   }
 
   return (
     <div className="users-container">
       <div className="users-list-wrapper">
         <h3 className="user-heading">List of Users</h3>
-        <div className="users-list-header">
-          <p className="count-heading">S/N</p>
-          <div className="list-heading">
-            <p className="list-head">Name of Users</p>
-            <p className="list-head" id='email-heading'>Email of Users</p>
-          </div>
-        </div>
         {filteredUsers?.map((user) => (
           <div className="users-list" key={user.id}>
             <p className="count">{user.id}.</p>
